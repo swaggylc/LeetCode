9c33eae91b133e674b63e94a34e2b3f78e41019a
lucan <lucanlc0417@gmail.com>
2023-08-07 20:20:00 +0800

    27.移除元素(双指针)

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -11,13 +11,27 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == val) {
-            nums.splice(i, 1)
-            i = -1  //从索引为0的位置再遍历
+    // 循环+splice
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] == val) {
+    //         nums.splice(i, 1)
+    //         i = -1  //从索引为0的位置再遍历
+    //     }
+    // }
+    // return nums.length
+
+    // 双指针
+    let slow = 0
+    for (let fast = 0; fast < nums.length; fast++) {
+        if (nums[fast] != val) {
+            nums[slow] = nums[fast]
+            slow++
         }
     }
-    return nums.length
+    return slow
+
+
+
 };
 // @lc code=end
 
