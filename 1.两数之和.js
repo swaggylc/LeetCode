a7bb180879aa21a2a189f862e23557ff82f82723
lucan <lucanlc0417@gmail.com>
2023-08-16 17:02:36 +0800

    1.两数之和(哈希表)

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -11,16 +11,27 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let arr = [];
+  // let arr = [];
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] == target) {
+  //       arr[0] = i;
+  //       arr[1] = j;
+  //       break
+  //     }
+  //   }
+  // }
+  // return arr;
+
+  let resultMap = new Map()
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        arr[0] = i;
-        arr[1] = j;
-        break
-      }
+    let res = target - nums[i]
+    if (resultMap.get(res) != undefined) {
+      return [resultMap.get(res), i]
     }
+    // 注意先查找再添加，否则在测试用例[3, 2, 4]，6下会输出[0,0]
+    // 因为6-3=3，输出[0,0]
+    resultMap.set(nums[i], i)
   }
-  return arr;
 };
 // @lc code=end
