2a3addf6d5c1f0ef0c2063b5492b8dcc5acf9662
lucan <lucanlc0417@gmail.com>
2023-08-18 19:59:38 +0800

    344.反转字符串

---
+++ 344.反转字符串.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=javascript
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    // reverse方法
+    return s.reverse()
+};
+// @lc code=end
+
